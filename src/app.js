d18ab0d79988d8e6e229a259e2bbbb2b5e4495a6
Idris Bowman <bowmanidris95@gmail.com>
2020-06-04 19:00:08 -0400

    setup tooling for nodemon and eslint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path =require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 // logging for incoming request (tiny, combined)
 app.use(morgan('tiny'));
@@ -16,11 +17,10 @@ app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/di
 app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(8080,()=> {
-    // Only runs when in debug mode
-    // In windows set DEBUG= * & node app.js
-    debug(`Listening on port ${chalk.green(8080)}`);
+app.listen(port, () => {
+  // In windows set DEBUG= * & node app.js
+  debug(`Listening at port ${chalk.green(port)}`);
 });
