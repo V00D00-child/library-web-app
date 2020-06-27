65082c4ff0069e73a195eaeb666a561b1853d76a
Idris Bowman <bowmanidris95@gmail.com>
2020-06-27 13:41:01 -0400

    add loacl authentcation straegy

--- src/middleware/strategies/local.strategy.js
+++ src/middleware/strategies/local.strategy.js
@@ -1,13 +1,35 @@
 const passport = require('passport');
 const { Strategy } = require('passport-local');
+const chalk = require('chalk');
+const debug = require('debug')('app:local.strategy');
 const AppUser = require('../../model/user');
+const pool = require('../databaseConfig');
 
 function localStrategy() {
   passport.use(new Strategy(
     { usernameField: 'userName', passwordField: 'password' },
     (userName, password, done) => {
-      const user = new AppUser(userName, password);
-      done(null, user);
+      (async function doGetAppUser() {
+        try {
+          const result = await pool.query('SELECT * FROM App_User WHERE User_Name = ?', [userName]);
+          debug(chalk.green('DataBase SELECT user:'), result[0]);
+
+          const user = new AppUser(
+            result[0].User_Id,
+            result[0].User_Name,
+            result[0].Encryted_Password,
+          );
+
+          if (user.password === password) {
+            debug(chalk.green('user password matches'));
+            done(null, user);
+          } else {
+            done(null, false);
+          }
+        } catch (error) {
+          debug(chalk.red(error));
+        }
+      }());
     },
   ));
 }
--- src/model/user.js
+++ src/model/user.js
@@ -1,5 +1,6 @@
 class Appuser {
-  constructor(username, password) {
+  constructor(id, username, password) {
+    this.id = id;
     this.username = username;
     this.password = password;
   }
